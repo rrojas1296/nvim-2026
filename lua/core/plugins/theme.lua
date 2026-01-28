15fcb9666c44d69d7a247c77c9e323a72e6256c9
Diego Rojas <rrojas1296@gmail.com>
2026-01-28 18:34:19 -0500

    master: Changed theme color

--- lua/core/plugins/theme.lua
+++ lua/core/plugins/theme.lua
@@ -4,8 +4,7 @@ return {
   priority = 1000,
   opts = {},
   config = function()
-    local theme = require("tokyonight")
     local colorschema = vim.cmd.colorscheme
-    colorschema("tokyonight-moon")
+    colorschema("tokyonight-night")
   end,
 }
