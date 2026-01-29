2e84773594a2b1c3ac5ba3bdc03bd86997d96202
diego rojas <devup2332@gmail.com>
2026-01-29 15:54:17 -0500

    master: Removed logs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,30 +1,29 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-print(laypath)
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 
 vim.opt.rtp:prepend(lazypath)
 local lazy = require("lazy")
 
 lazy.setup({
-	spec = {
-		{ import = "plugins" },
-		{ import = "plugins.lsp" },
-	},
-	install = { colorscheme = { "tokynight-moon" } },
-	checker = { enabled = false },
+  spec = {
+    { import = "plugins" },
+    { import = "plugins.lsp" },
+  },
+  install = { colorscheme = { "tokynight-moon" } },
+  checker = { enabled = false },
 })
 
 vim.cmd.colorscheme("solarized-osaka")
